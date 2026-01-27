ce92ee83abf46f4ff1577a6e41c182e3ef1be1c3
idilcansu <idil.cansu.sahin@gmail.com>
2026-01-27 14:52:53 +0300

    feat(app): add auth_api and update pubspec

---
+++ ui_prototype/lib/features/auth/services/auth_api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AuthApi {
+  // Android Emulator: PC'deki localhost'a ulaşmak için 10.0.2.2 kullanılır
+  static const String baseUrl = "http://10.0.2.2:3000/api";
+
+  static Future<Map<String, dynamic>> login({
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse("$baseUrl/auth/login");
+
+    final res = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": email, "password": password}),
+    );
+
+    final data = jsonDecode(res.body);
+
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      return data; // { message, token, user }
+    } else {
+      throw Exception(data["message"] ?? "Login failed");
+    }
+  }
+}
