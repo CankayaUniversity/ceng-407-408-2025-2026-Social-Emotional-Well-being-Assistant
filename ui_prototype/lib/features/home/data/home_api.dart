6a53885fcda50446b54a1c78287fc9c6c6a9ee78
idilcansu <idil.cansu.sahin@gmail.com>
2026-03-31 20:37:26 +0300

    update home api integration

--- ui_prototype/lib/features/home/data/home_api.dart
+++ ui_prototype/lib/features/home/data/home_api.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import '../../core/api/api_client.dart';
+import '../../../core/api/api_client.dart';
 
 class HomeApi {
   static Future<List<String>> getSelectedTabs() async {
